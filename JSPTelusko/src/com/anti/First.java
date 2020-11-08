e7c73579ccfa9d5c1453ddb777db4b120abf26b7
gyepi <antipet@gmail.com>
2020-11-08 23:53:16 +0100

    commit jsp servlet 20201008

---
+++ JSPTelusko/src/com/anti/First.java
@@ -0,0 +1,29 @@
+package com.anti;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class First
+ */
+@WebServlet("/First")
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        Cookie[] cks = request.getCookies();
+        out.println("Welcome "+ cks[0].getValue());
+	}
+
+
+}
---
+++ JSPTelusko/src/com/anti/TestServletContext.java
@@ -0,0 +1,71 @@
+package com.anti;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class ServletContext
+ */
+@WebServlet(
+		
+		name = "servletParamAnnotation", 
+		urlPatterns =  {"/ServletContext"},
+		
+		initParams =
+	        {
+	            @WebInitParam(name = "saveDir", value = "D:/FileUpload"),
+	            @WebInitParam(name = "allowedTypes", value = "jpg,jpeg,gif,png")
+	        }
+		)
+
+
+
+public class TestServletContext extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: dddd ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+	    String name = request.getParameter("user");
+	    String pass = request.getParameter("pass");
+	        
+	    
+	    PrintWriter out = response.getWriter();
+	    ServletContext ctx = getServletContext();
+	    out.println("Servlet Context:");
+	    out.println("<br/>");
+	    out.println(ctx.getInitParameter("name"));
+	    out.println(ctx.getInitParameter("consol"));
+	    out.println("</br>");
+	    
+	    String saveDir = getInitParameter("saveDir");
+        String fileTypes = getInitParameter("allowedTypes");
+        
+        out.println("saveDir = " + saveDir);
+        out.println("</br>");
+        out.println("fileTypes = " + fileTypes);
+	        if(pass.equals("1234"))
+	        {
+	            Cookie ck = new Cookie("username", name);
+	            response.addCookie(ck);
+	            response.sendRedirect("First");
+	        }
+	        
+	        
+	}
+
+	
+	
+
+}
