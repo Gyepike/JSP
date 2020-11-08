5983d9bcb284b883c942099b8a4289e71288745c
gyepi <antipet@gmail.com>
2020-11-08 23:58:44 +0100

    commit jsp servlet 20201008 final

--- JSPTelusko/src/com/anti/TestServletContext.java
+++ JSPTelusko/src/com/anti/TestServletContext.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(
 		
 		name = "servletParamAnnotation", 
-		urlPatterns =  {"/ServletContext"},
+		urlPatterns =  {"/ServletContext", "/Kuki"},
 		
 		initParams =
 	        {
