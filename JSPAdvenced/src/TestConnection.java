8c6270f27bbf6820059cc771980204760c44374d
gyepi <antipet@gmail.com>
2020-11-11 23:37:24 +0100

    commit jsp servlet 20201111 final data test

--- JSPAdvenced/src/TestConnection.java
+++ JSPAdvenced/src/TestConnection.java
@@ -9,6 +9,14 @@ import java.sql.*;
  * process results 
  * close 
  * 
+ * Statment type
+ * 
+ *  Nomalma stament + varibale bad
+ *  PreperedStament 
+ * 
+ *  CallableStatement PL/SQL Stroe oprocedure 
+ * 
+ * 
  */
 
 public class TestConnection {
@@ -20,12 +28,25 @@ public class TestConnection {
 			 String uname = "hbstudent";
 			 String pass = "hbstudent";
 			 
-			 String query = "SELECT * FROM Alien.aliens where idaliens = 1";
+			 int  aid = 7;
+			 String name = "Ali";
+			 String skill = "Repuloszonyeg";
+			 
+			 int  aid2 = 8;
+			 String name2 = "Ali";
+			 String skill2 = "Repuloszonyeg";
+			 
+			 
+			 String query = "SELECT * FROM Alien.aliens where idaliens = 2";
+			 String query2 = "SELECT * FROM Alien.aliens";
+			 String query3 = "INSERT INTO Alien.aliens VALUES ("+ aid + ", '"+name+"', '"+skill +"')";  // normal way 			 
+			 String query4 = "INSERT INTO Alien.aliens VALUES  (?, ?, ?) ";  // prepered statment 
+			 
 			 Class.forName("com.mysql.jdbc.Driver"); // class formname telusko
 	         Connection con = DriverManager.getConnection(url,uname,pass);
-			 Statement stat = con.createStatement();
+			 Statement stat = con.createStatement(); // preperedStament
 	
-			 ResultSet res =  stat.executeQuery(query);
+			 ResultSet res =  stat.executeQuery(query2);  
 			 
 			 while(res.next()) {
 				 
@@ -33,9 +54,26 @@ public class TestConnection {
 				 String tech  = res.getString("tech");
 				 int    id  = res.getInt(1);
 				 				 
-				 System.out.println("id. " + id +  "   Alien neve " + aname+ " " + " A technologia " + tech );
+				 System.out.println("id. " + id +  "  Alien neve: " + aname+ " " + "A technologia " + tech );
 			 }
 			 
+			 res =  stat.executeQuery(query);
+			 res.next();
+			 System.out.println(res.getString(2));
+			 
+			int count = stat.executeUpdate(query3); // normal stament
+			System.out.println("A table "+count+" db rekord(ok) update");
+			 
+			 
+		     PreparedStatement pstat = con.prepareStatement(query4); // prepered statment 
+		     pstat.setInt(1, aid2);
+		     pstat.setString(2, name2);
+		     pstat.setString(3, skill2);
+		     
+		     count = pstat.executeUpdate();
+		     System.out.println("A table "+count+" db rekord(ok) update");
+			 stat.close();
+			 con.close();
 			
 		} catch (Exception e) {
 			System.out.println(e);
