e906e965738e09db37f89085b8812da62dcd3e52
gyepi <antipet@gmail.com>
2020-11-11 21:37:09 +0100

    commit jsp servlet 20201111 final data test

--- JSPAdvenced/src/TestConnection.java
+++ JSPAdvenced/src/TestConnection.java
@@ -19,8 +19,23 @@ public class TestConnection {
 			 String url = "jdbc:mysql://localhost:3306/Alien?serverTimezone=UTC&useSSL=false";
 			 String uname = "hbstudent";
 			 String pass = "hbstudent";
+			 
+			 String query = "SELECT * FROM Alien.aliens where idaliens = 1";
 			 Class.forName("com.mysql.jdbc.Driver"); // class formname telusko
 	         Connection con = DriverManager.getConnection(url,uname,pass);
+			 Statement stat = con.createStatement();
+	
+			 ResultSet res =  stat.executeQuery(query);
+			 
+			 while(res.next()) {
+				 
+				 String aname  = res.getString("aname");
+				 String tech  = res.getString("tech");
+				 int    id  = res.getInt(1);
+				 				 
+				 System.out.println("id. " + id +  "   Alien neve " + aname+ " " + " A technologia " + tech );
+			 }
+			 
 			
 		} catch (Exception e) {
 			System.out.println(e);
