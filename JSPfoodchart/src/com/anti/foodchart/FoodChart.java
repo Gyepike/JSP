44590b3f0bb6ac26b5cb7d7a18c3de6027ba7887
gyepi <antipet@gmail.com>
2021-01-12 22:21:18 +0100

    correct maven project 20210112

--- JSPfoodchart/src/com/anti/foodchart/FoodChart.java
+++ JSPfoodchart/src/com/anti/foodchart/FoodChart.java
@@ -13,39 +13,39 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FoodChart
  */
+
 @WebServlet("/FoodChart")
 public class FoodChart extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-        FoodCastDButil food = new FoodCastDButil();
+
+	private void list(HttpServletRequest request) {
+
+		FoodCastDButil food = new FoodCastDButil();
 		try {
 			List<Food> list = food.getFoodList();
 			request.setAttribute("list", list);
-			
+
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+
 			e.printStackTrace();
 		}
-	
-		// get data from modell servlet is the controller
-		
-		String[] foodItems = {"Apple Pie", "Cola", "Hamburger", "Pizza"};  
-		// redirect to viwe jsp page !! 
-		
-	    String name = "Munyi&Munyo";
-	    		
-		RequestDispatcher req =   request.getRequestDispatcher("foodlist.jsp");
-	    
-	    
-		request.setAttribute("foodItemsList", foodItems);
-		request.setAttribute("Name", name);	
-		
-	    
-        req.forward(request, response);
+
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String[] foodItems = { "Apple Pie", "Cola", "Hamburger", "Pizza" };
+		
+		// redirect to viwe jsp page !!
+		list(request);
+
+		String name = "Munyi&Munyo";
+
+		RequestDispatcher req = request.getRequestDispatcher("foodlist.jsp");
+
+		request.setAttribute("foodItemsList", foodItems);
+		request.setAttribute("Name", name);
+		req.forward(request, response);
+	}
 }
